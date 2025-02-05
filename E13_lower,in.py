abcf3043c3b16010c51c8c6e7bc122ccefdc0ddf
AndresFForeroP <andres2200500@correo.uis.edu.co>
2025-02-04 19:20:23 -0500

    12 terminado 13 iniciado

---
+++ E13_lower,in.py
@@ -0,0 +1,19 @@
+'''
+Autor: Andres Forero
+Fecha: 03/02/2025
+Descripcion: uso de lower y de in en diccionarios 
+'''
+def agregar_contactos():
+    nombre = input('ingrese el nombre del contacto ')
+    while True:
+        try:
+            telefono = int(input(f'Ingrese el numero de telefono de {nombre} ')) 
+        except Exception:
+            print('ERROR,solo ingrese numeros')
+        else:
+            break
+    nContacto = {nombre,telefono}
+    contactos.append(nContacto)
+    print(contactos)    
+contactos = []
+agregar_contactos()
